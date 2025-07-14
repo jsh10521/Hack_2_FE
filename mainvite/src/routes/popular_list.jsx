5b6f2da2ebb089778db28065d971cca178ed42ba
jsh10521 <jsh10521@naver.com>
2025-07-14 21:22:45 +0900

    수정_5

--- mainvite/src/routes/popular_list.jsx
+++ mainvite/src/routes/popular_list.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ClickableBox from "../component/ClickableBox";
 import PopularMovies from "../Home/PopularMovies";
-import nice from "../assets/다운로드.jpeg"
 
 export default function PopularList() {
   return (
     <div style={{ display: "flex", gap: "20px", padding: "20px" }}>
       <ClickableBox
-        imageSrc={nice}
-        to="/detail_list"
+        imageSrc=""
+        to="/detail_list/1"
         title="영화 이름"
       />
       <ClickableBox
